fa29c4f42f502a37249735489f96c4d4f8d9dd19
wyc <864147974@qq.com>
2024-12-19 15:32:58 +0800

    fix

--- docker/run_scene.py
+++ docker/run_scene.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import sys
 import caffe
@@ -20,28 +22,43 @@ def classify_scene(fpath_design, fpath_weights, fpath_labels, im):
 	# since we classify only one image, we change batch size from 10 to 1
 	net.blobs['data'].reshape(1,3,227,227)
 
+	t0 = time.time()
+	print "start: " + str(t0)
+
 	# load the image in the data layer
 	net.blobs['data'].data[...] = transformer.preprocess('data', im)
 
 	# compute
 	out = net.forward()
 
-	# print top 5 predictions - TODO return as bytearray?
-	with open(fpath_labels, 'rb') as f:
+	t1 = time.time()
+	print "end: " + str(t1)
+	print "cost: " + str(t1 - t0) + " seconds"
 
-		labels = pickle.load(f)
+	# print top 5 predictions - TODO return as bytearray?
+	# with open(fpath_labels, 'rb') as f:
+	#
+	# 	labels = pickle.load(f)
+	# 	top_k = net.blobs['prob'].data[0].flatten().argsort()[-1:-6:-1]
+	#
+	# 	for i, k in enumerate(top_k):
+	# 		print i, labels[k]
+
+	with open(fpath_labels, 'r') as f:
+
+		labels = f.readlines()
 		top_k = net.blobs['prob'].data[0].flatten().argsort()[-1:-6:-1]
-		
+
 		for i, k in enumerate(top_k):
 			print i, labels[k]
-
 
 if __name__ == '__main__':
 
 	# fetch pretrained models
 	fpath_design = 'models_places/deploy_alexnet_places365.prototxt'
 	fpath_weights = 'models_places/alexnet_places365.caffemodel'
-	fpath_labels = 'resources/labels.pkl'
+	# fpath_labels = 'resources/labels.pkl'
+	fpath_labels = 'resources/labels2.txt'
 
 	# fetch image
 	im = caffe.io.load_image(sys.argv[1])
